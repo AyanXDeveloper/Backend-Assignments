621a2c8f38d78940605752198f0879dec9363219
AyanXDeveloper <ayyanzaveri@gmail.com>
2026-06-03 11:40:13 +0500

    Backend Assignment

---
+++ Server Creation Practice/server.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const http = require('http');
+const path = require('path');
+
+// <==== Creating Server ====>
+
+const server = http.createServer((req, res) => {
+
+    console.log(`Request for ${req.url} received.`);
+
+    if (req.url === '/') {
+        res.end('Welcome to the homepage!');
+    } else if (req.url === '/about') {
+        res.end('Welcome to the about page!');
+    } else if (req.url === '/contact') {
+        res.end('Welcome to the contact page!');
+    } else if (req.url === '/services') {
+        res.end('Welcome to the services page!');
+    } else if (req.url === '/products') {
+        res.end('Welcome to the products page!');
+    } else {
+        res.writeHead(404, {
+            "Content-type": "text/html"
+        })
+        res.end("This page does not exist")
+    }
+});
+
+// <==== Running Server ====>
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+
+const readFile = fs.readFileSync("random.txt", "utf-8")
+console.log(readFile);
+
+fs.writeFileSync("abc.txt", "This is a new file created using fs module in Node.js named abc.txt")
+
+const readTextFile = fs.readFileSync("abc.txt", "utf-8")
+console.log(readTextFile);
+
+fs.readFile("random.txt", "utf-8", (err, data) => {
+
+    if (err) {
+        console.log(err);
+        return;
+    } else if (data) {
+        return console.log(data);
+    }
+
+})
